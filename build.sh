f43f9dfc1992c0f671bca728f92a156bc9c9aecb
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2026-07-14 22:25:58 +0300

    Update

--- build.sh
+++ build.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-BUILD_DIR="$(pwd)/.pyinstaller.out"
+PROJECT_ROOT_DIR="$(pwd)"
+TEST_DIR="$PROJECT_ROOT_DIR/tests/blueprint/project"
+
+BUILD_DIR="$PROJECT_ROOT_DIR/.pyinstaller.out"
 DIST_DIR="$BUILD_DIR/dist"
 WORK_DIR="$BUILD_DIR/build"
-MAIN_FILE="$(pwd)/src/app/__main__.py"
+RELEASE_DIR="$BUILD_DIR/dist/blueprintcpp/"
+RELEASE_NAME="blueprintcpp"
+MAIN_FILE="$PROJECT_ROOT_DIR/src/app/__main__.py"
 
-TEMPLATES_DIR="$(pwd)/src/app/jinja/templates"
+TEMPLATES_DIR="$PROJECT_ROOT_DIR/src/app/jinja/templates"
 TEMPLATES_PACKAGE_DIR="app/jinja/templates"
 
 RELEASE_ZIP_NAME="blueprintcpp-linux-x86_64.zip"
-RELEASE_PATH="$BUILD_DIR/dist/blueprintcpp/"
-RELEASE_NAME="blueprintcpp"
 
-pyinstaller --onedir                                            \
-            --noconfirm                                         \
-            --name="$RELEASE_NAME"                              \
+# Build project
+pyinstaller --onedir --noconfirm                                \
+            --name     "$RELEASE_NAME"                          \
             --add-data "$TEMPLATES_DIR:$TEMPLATES_PACKAGE_DIR"  \
             --distpath "$DIST_DIR"                              \
             --workpath "$WORK_DIR"                              \
             --specpath "$BUILD_DIR"                             \
             "$MAIN_FILE"
 
+# ZIP release
 cd "$DIST_DIR"
 zip -r -X "$RELEASE_ZIP_NAME" "$RELEASE_NAME"
+
+# Run program to generate from test files
+cd "$PROJECT_ROOT_DIR"
+"$RELEASE_DIR/$RELEASE_NAME" "$TEST_DIR"
